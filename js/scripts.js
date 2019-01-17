37138c5affdfc98330f2352e1d031491499834b6
Riley Benson <ryguybenson@yahoo.com>
2019-01-16 18:48:32 -0800

    attempt to make inputs submit but not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var nameInput = $("input#name").val();
+    var music = parseInt($("#music")).val();
+    var food = $("input:radio[name=food-type]:checked").val();
+    var color = $("#color").val();
 
     $(".name").text(nameInput);
+    $(".music").text(music);
+    $(".food").text(food);
+    $(".color").text(color);
 
     $("#results").show();
 
